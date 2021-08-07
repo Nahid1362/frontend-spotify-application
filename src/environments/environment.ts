31f1b93fc4ee1bf47b187692f2f1974b39abdc19
Nahid <69681542+Nahid1362@users.noreply.github.com>
2021-08-07 09:33:52 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   clientID: "94a46c1add6e4caf9d092f3fa7006718",
   clientSecret:"a7226708dc274c6cb33c2ef2b6ce2573",
   // userAPIBase:"http://localhost:8080/api/user"
-  userAPIBase:"https://web422nahid.herokuapp.com/api/user"
+  userAPIBase:"https://lit-fortress-70043.herokuapp.com/api/user"
 };
 
 /*
